9f0689607baa2039ba285e37de7dc60d2042d35c
hailay12 <hailaymk@gmail.com>
2016-09-12 17:53:52 +0300

    Update about.php

--- about.php
+++ about.php
@@ -1,6 +1,6 @@
 <html>
 <body>
 <h2> this is my first por</h2>
-<a href="index.php"> Home </a>
+<a href="http://htmlpreview.github.io/?https://github.com/yibrah/MIT-Projects/master/about.php"> Home </a>
 </body>
 </html>
