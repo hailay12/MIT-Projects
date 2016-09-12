8cf4adeeb16148e1dbfe54b7afb7e0ded1fa7223
hailay12 <hailaymk@gmail.com>
2016-09-12 18:12:54 +0300

    Update about.php

--- about.php
+++ about.php
@@ -1,6 +1,19 @@
+<!DOCTYPE html>
 <html>
 <body>
 <h2> This is About</h2>
 <a href="http://htmlpreview.github.io/?https://github.com/yibrah/MIT-Projects/master/index.php">Home</a>
+<form>
+  First name:<br>
+  <input type="text" name="firstname">
+  <br>
+  Last name:<br>
+  <input type="text" name="lastname">
+</form>
+
+<p>Note that the form itself is not visible.</p>
+
+<p>Also note that the default width of a text input field is 20 characters.</p>
+
 </body>
 </html>
