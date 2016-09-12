48aeb40e97316ce1573ed4d3d9630ec280a004c1
hailay12 <hailaymk@gmail.com>
2016-09-12 18:03:48 +0300

    Update about.php

--- about.php
+++ about.php
@@ -1,6 +1,9 @@
 <html>
 <body>
 <h2> This is About</h2>
-<a href="http://htmlpreview.github.io/?https://github.com/yibrah/MIT-Projects/master/index.php">Home</a>
+<a href="http://htmlpreview.github.io/?https://github.com/yibrah/MIT-Projects/master/index.php">Home</a> |
+  <a href="http://htmlpreview.github.io/?https://github.com/yibrah/MIT-Projects/master/about.php">About</a> | 
+  <a href="http://htmlpreview.github.io/?https://github.com/yibrah/MIT-Projects/master/services.php">Services</a> | 
+  <a href="http://htmlpreview.github.io/?https://github.com/yibrah/MIT-Projects/master/contact.php">Contact</a>
 </body>
 </html>
